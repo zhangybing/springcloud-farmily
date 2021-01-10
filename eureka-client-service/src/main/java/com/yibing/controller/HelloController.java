9e3db277fb7dc685ab7f2874856aee94d12f3cff
1471405994@qq.com <1471405994@qq.com>
2021-01-11 07:58:33 +0800

    引入Ribbon的服务调用失败重试机制

--- eureka-client-service/src/main/java/com/yibing/controller/HelloController.java
+++ eureka-client-service/src/main/java/com/yibing/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author yibing
@@ -15,10 +16,23 @@ public class HelloController {
     @Value("${server.port}")
     String port;
 
+    private AtomicInteger count = new AtomicInteger();
+
     @RequestMapping("/sayHello")
     public String sayHello(HttpServletRequest request) {
+        /**
+         * 这里设置等待时间，是为了测试ribbon的超时机制
+         */
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int i = count.incrementAndGet();
         String id = request.getParameter("id");
         String name = request.getParameter("name");
+        System.out.println("第"+count+"次调用");
         return port + ": hello spring cloud :---->"+id+"------"+name;
     }
 
